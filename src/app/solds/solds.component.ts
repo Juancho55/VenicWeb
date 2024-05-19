5773a881f7e1c184f5ac3072b802d0bb01229e40
juandalayva@gmail.com <juandalayva@gmail.com>
2024-05-19 17:03:15 -0500

    new pang

---
+++ src/app/solds/solds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solds',
+  standalone: true,
+  imports: [],
+  templateUrl: './solds.component.html',
+  styleUrl: './solds.component.css'
+})
+export class SoldsComponent {
+
+}
